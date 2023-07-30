50bb1ffa7693499cc297a6999bac456a02fd688e
Ed <mail@544062.ru>
2023-07-30 17:30:56 +0300

    feat: add api annotations user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class User {
+  @ApiProperty()
   id: string; // uuid v4
+
+  @ApiProperty()
   login: string;
+
+  @ApiProperty()
   password: string;
+
+  @ApiProperty()
   version: number; // integer number, increments on update
+
+  @ApiProperty()
   createdAt: number; // timestamp of creation
+
+  @ApiProperty()
   updatedAt: number; // timestamp of last update
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,25 +10,60 @@ import {
   NotFoundException,
   ParseUUIDPipe,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiParam,
+  ApiNotFoundResponse,
+  ApiNoContentResponse,
+} from '@nestjs/swagger';
+
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
   @Get()
+  @ApiOperation({
+    summary: 'get all users',
+  })
+  @ApiOkResponse({
+    description: 'Users successfully getted',
+    type: [User],
+  })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'get single user by id',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Id of user',
+    type: 'string',
+  })
+  @ApiOkResponse({
+    description: 'User successfully getted',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'User id is invalid (not UUID)',
+  })
+  @ApiNotFoundResponse({
+    description: 'User with given id not found',
+  })
   findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     const user = this.userService.findOne(id);
     if (!user) {
@@ -37,7 +72,47 @@ export class UserController {
     return user;
   }
 
+  @Post()
+  @ApiOperation({
+    summary: 'create user',
+  })
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Data for new user',
+  })
+  @ApiCreatedResponse({
+    description: 'User successfully created',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'Request body does not contain required fields',
+  })
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
   @Put(':id')
+  @ApiOperation({
+    summary: 'update user password',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Id of user',
+    type: 'string',
+  })
+  @ApiBody({
+    type: UpdatePasswordDto,
+    description: 'Data for update user',
+  })
+  @ApiOkResponse({
+    description: 'User successfully updated',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'User id is invalid (not UUID)',
+  })
+  @ApiNotFoundResponse({ description: 'User with given id not found' })
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateUserDto: UpdatePasswordDto,
@@ -50,6 +125,22 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'delete user',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Id of user',
+    type: 'string',
+  })
+  @ApiNoContentResponse({
+    description: 'User successfully deleted',
+  })
+  @ApiBadRequestResponse({
+    description: 'User id is invalid (not UUID)',
+  })
+  @ApiNotFoundResponse({ description: 'User with given id not found' })
   @HttpCode(204)
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.userService.remove(id);
