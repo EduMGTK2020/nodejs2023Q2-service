7ec39ca83144c5ce27b7ab8bc65fc24ff88c1184
Ed <mail@544062.ru>
2023-08-19 17:20:26 +0300

    feat: add rotate files for logger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,20 +1,45 @@
 import { Injectable, ConsoleLogger, Scope } from '@nestjs/common';
 import * as fs from 'fs';
-
-const logDir = process.env.APP_LOG_DIR || 'logs';
+import * as path from 'path';
 
 @Injectable({ scope: Scope.REQUEST })
 export class LoggerService extends ConsoleLogger {
-  private readonly fileName = `./${logDir}/logs.txt`;
+  private readonly fileToLogName = path.join(
+    `./${process.env.APP_LOG_DIR || 'logs'}`,
+    'log.txt',
+  );
+
+  private readonly maxFileSize =
+    1024 * ((+process.env.APP_LOG_FILESIZE || 30) - 1);
 
   logMessage(type: string, message: string) {
     const messageToLog = `[${new Date().toISOString()}] ${type} ${message}\n`;
+
     process.stdout.write(messageToLog);
-    fs.appendFile(this.fileName, messageToLog, (err) => {
+
+    this.logRotate();
+
+    fs.appendFile(this.fileToLogName, messageToLog, (err) => {
       if (err) {
         console.error(err);
       }
     });
+  }
+
+  private logRotate() {
+    try {
+      const fileSize = fs.statSync(this.fileToLogName).size;
+      if (fileSize >= this.maxFileSize) {
+        const backupFileName = this.fileToLogName.replace(
+          'log.txt',
+          `log-${Date.now()}.txt`,
+        );
+        fs.renameSync(this.fileToLogName, backupFileName);
+        fs.openSync(this.fileToLogName, 'w');
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   log(message: string) {
