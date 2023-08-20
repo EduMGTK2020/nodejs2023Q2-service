78f65202f70484b20ea33a874a348f1b0ff9394f
Ed <mail@544062.ru>
2023-08-20 10:58:25 +0300

    feat: add error log to separate file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -9,6 +9,11 @@ export class LoggerService extends ConsoleLogger {
     'log.txt',
   );
 
+  private readonly fileToErrorName = path.join(
+    `./${process.env.APP_LOG_DIR || 'logs'}`,
+    'error.txt',
+  );
+
   private readonly maxFileSize =
     1024 * ((+process.env.APP_LOG_FILESIZE || 30) - 1);
 
@@ -24,10 +29,18 @@ export class LoggerService extends ConsoleLogger {
         console.error(err);
       }
     });
+
+    if (type == 'ERROR') {
+      fs.appendFile(this.fileToErrorName, messageToLog, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
   }
 
   private logRotate() {
-    try {
+    if (fs.existsSync(this.fileToLogName)) {
       const fileSize = fs.statSync(this.fileToLogName).size;
       if (fileSize >= this.maxFileSize) {
         const backupFileName = this.fileToLogName.replace(
@@ -37,13 +50,22 @@ export class LoggerService extends ConsoleLogger {
         fs.renameSync(this.fileToLogName, backupFileName);
         fs.openSync(this.fileToLogName, 'w');
       }
-    } catch (err) {
-      console.error(err);
+    }
+    if (fs.existsSync(this.fileToErrorName)) {
+      const fileErrSize = fs.statSync(this.fileToErrorName).size;
+      if (fileErrSize >= this.maxFileSize) {
+        const backupFileName = this.fileToErrorName.replace(
+          'error.txt',
+          `error-${Date.now()}.txt`,
+        );
+        fs.renameSync(this.fileToErrorName, backupFileName);
+        fs.openSync(this.fileToErrorName, 'w');
+      }
     }
   }
 
-  log(message: string) {
-    this.logMessage('LOG', message);
+  log(message: string, context?: string) {
+    this.logMessage('LOG', message + ' - ' + context);
   }
 
   error(message: string) {
